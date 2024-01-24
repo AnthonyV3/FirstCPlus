4ee38c064567e35014409198e50a09f15d759ce3
AnthonyyV <avargas55@students.avc.edu>
2024-01-24 11:10:49 -0800

    print

---
+++ cs120Firstcoding/cs120Firstcoding/cs120FirstcodingApp.swift
@@ -0,0 +1,32 @@
+//
+//  cs120FirstcodingApp.swift
+//  cs120Firstcoding
+//
+//  Created by Admin on 1/24/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct cs120FirstcodingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
